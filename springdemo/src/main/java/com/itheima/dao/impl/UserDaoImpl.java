943c8488b884eed47a66628db2f5d0c590688410
vi835 <vi835@foxmail.com>
2020-11-23 07:09:47 +0800

    commit

--- springdemo/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
+++ springdemo/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
@@ -3,6 +3,17 @@ package com.itheima.dao.impl;
 import com.itheima.dao.UserDao;
 
 public class UserDaoImpl implements UserDao {
+    public UserDaoImpl() {
+        System.out.println("UserDaoImpl创建 ...");
+    }
+
+    public void init(){
+        System.out.println("初始化方法 ...");
+    }
+
+    public void destroy(){
+        System.out.println("销毁方法 ...");
+    }
     public void save() {
         System.out.println("save running ...");
     }
